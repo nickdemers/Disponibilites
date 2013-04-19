3802935a72aa3f30fa2801af8aa4138aa076b926
Nicolas Demers <nickdemers@gmail.com>
2013-04-18 23:06:07 -0400

    Page accueil modifiée, connexion automatique à Google API

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,27 @@
 class HomeController < ApplicationController
   def index
+
+    if session[:token].nil?
+
+      redirect_to "/auth/google_oauth2"
+
+    else
+
+      client = Google::APIClient.new
+      client.authorization.access_token = session[:token]
+      service = client.discovered_api('calendar', 'v3')
+
+      result = client.execute(:api_method => service.events.list,
+                               :parameters => {'calendarId' => 'nickdemers@gmail.com'})
+
+      if !result.nil?
+
+        @events_calendar = result.data.items
+
+        @events_calendar.each do |e|
+          puts e.summary
+        end
+      end
+    end
   end
 end
---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,11 @@
+#encoding=utf-8
+require 'spec_helper'
+
+describe HomeController do
+
+  it "utilisateur non connecté, rediriger vers la page de loggin"
+
+  it "" do
+
+  end
+end
